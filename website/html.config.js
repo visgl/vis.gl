46a47b747f6c16d01a734b39906cc234c339c087
Balthazar Gronon <balthazar@uber.com>
2017-09-21 14:05:26 -0700

    baseHref attempt

--- website/html.config.js
+++ website/html.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   title: "Uber's Visualization Frameworks",
 
+  baseHref: process.env.NODE_ENV === 'production' ? '~ib/vis/' : '/',
+
   meta: [
     {
       name: 'description',
