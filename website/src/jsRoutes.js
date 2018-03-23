ddb8cf17ad7e4735a762b2ed3139471f55a7a433
sectoreight <35932844+sectoreight@users.noreply.github.com>
2018-03-23 16:04:57 -0700

    Added 'In The News' page. Extracted relevant content from 'About' page (#15)
    lgtm

--- website/src/jsRoutes.js
+++ website/src/jsRoutes.js
@@ -1,8 +1,13 @@
-import About from 'components/About'
+import About from "components/About";
+import InTheNews from "components/InTheNews";
 
 export default [
   {
-    path: '/about',
-    component: About,
+    path: "/about",
+    component: About
   },
-]
+  {
+    path: "/inthenews",
+    component: InTheNews
+  }
+];
