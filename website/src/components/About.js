2a853c055b5f9a76a28013fc47762015c6e68e4b
Jerome Cukier <jerome@uber.com>
2018-05-04 15:51:15 -0700

    bump-ocular

--- website/src/components/About.js
+++ website/src/components/About.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React from 'react'
 
-import Footer from "components/Footer";
-import LinkWithArrow from "components/LinkWithArrow";
+import Footer from 'components/Footer'
+import LinkWithArrow from 'components/LinkWithArrow'
 
 function About() {
   return (
@@ -9,10 +9,10 @@ function About() {
       <Intro />
       <Footer />
     </div>
-  );
+  )
 }
 
-export default About;
+export default About
 
 function Intro() {
   return (
@@ -20,8 +20,8 @@ function Intro() {
       <div className="section-title">
         <h2>About us</h2>
         <p>
-          We’re a team of software engineers, information designers, and
-          computer graphics specialists at Uber.
+          We’re a team of software engineers, information designers, and computer graphics
+          specialists at Uber.
         </p>
       </div>
       <div className="about--intro--pictures">
@@ -36,8 +36,8 @@ function Intro() {
       <div className="section-title section-title--no-logo mission">
         <h2>Our mission</h2>
         <p>
-          Enhance people’s ability to understand and communicate data through
-          interactive systems for data visualization and analysis.
+          Enhance people’s ability to understand and communicate data through interactive systems
+          for data visualization and analysis.
         </p>
       </div>
       <div className="about--intro--pictures">
@@ -46,33 +46,30 @@ function Intro() {
             className="about--pic"
             style={{ backgroundImage: "url('./images/team/bowling.png')" }}
           />
-          <p>{"Bowling night"}</p>
+          <p>{'Bowling night'}</p>
         </div>
         <div className="about--intro--pictures--small">
           <div
             className="about--pic"
             style={{ backgroundImage: "url('./images/team/hiking.png')" }}
           />
-          <p>{"Hiking Mt Tam"}</p>
+          <p>{'Hiking Mt Tam'}</p>
         </div>
         <div className="m-top">
           <div
             className="about--pic"
             style={{
               backgroundImage: 'url("./images/team/museum.png")',
-              height: "calc((100vw - 71px) / 3.34)",
-              maxHeight: 350
+              height: 'calc((100vw - 71px) / 3.34)',
+              maxHeight: 350,
             }}
           />
-          <p>{"Family photo at the California Academy of Science"}</p>
+          <p>{'Family photo at the California Academy of Science'}</p>
         </div>
       </div>
       <div className="big-link">
-        <LinkWithArrow
-          label="Join our team!"
-          url="https://www.uber.com/careers/list/20242/"
-        />
+        <LinkWithArrow label="Join our team!" url="https://www.uber.com/careers/list/13203/" />
       </div>
     </div>
-  );
+  )
 }
