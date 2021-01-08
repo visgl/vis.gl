070e532991ee49a9d5ea2280fa1630bb88ec4f46
Ib Green <ib@unfolded.ai>
2021-01-08 10:29:48 -0800

    Add overview of vis.gl releases

--- src/components/manifesto.js
+++ src/components/manifesto.js
@@ -13,14 +13,57 @@ export default function Manifesto() {
       Join the open planning meetings.
 
       <h1>History</h1>
-      2015 - The core deck.gl framework was developed by Uber from 2015 to support a wide range of geospatial visualization use cases across the company, and was almost immediately open sourced. <br />
-      2016 - luma.gl was created. <br />
-      2017 - math.gl was created. <br />
+      2020 - vis.gl Open Governance meetings started, under the auspices of the Linux Foundation and the UCF. <br />
+      2020 - Uber transferred a set of core vis.gl frameworks to the UCF. <br />
+      2019 - Uber created the Urban Computing Foundation (a sub-foundation of the Linux Foundation), transferring kepler.gl. <br />
       2018 - Uber open sourced kepler.gl <br />
       2018 - loaders.gl and nebula.gl were created. <br />
-      2019 - Uber created the Urban Computing Foundation (a sub-foundation of the Linux Foundation), transferring kepler.gl. <br />
-      2020 - Uber transferred a set of core vis.gl frameworks to the UCF. <br />
-      2020 - vis.gl Open Governance meetings started, under the auspices of the Linux Foundation and the UCF. <br />
+      2017 - math.gl was created. <br />
+      2016 - luma.gl was created. <br />
+      2015 - The core deck.gl framework was developed by Uber from 2015 to support a wide range of geospatial visualization use cases across the company, and was almost immediately open sourced. <br />
+
+      <h1>Open Governance</h1>
+
+      <h2>vis.gl 8.4-8.5 Community Planning</h2>
+      <h2>vis.gl 8.2-8.3 Community Planning</h2>
+
+
+      <h1>What is New?</h1>
+
+      <h2>vis.gl 8.4 Highlights</h2>
+
+      Release Date: TBD: Jan 2020
+      New versions: `deck.gl` 8.4, `luma.gl` 8.4, `react-map-gl`
+      Fixes only: `loaders.gl` 2.3 
+
+      - BaseMaps
+        - `react-map-gl` now supports Mapbox GL JS v2, Mapbox GL JS v1, and MapLibre renderers.
+        - `bing` maps??? 
+
+      - TypeScript 
+        - `luma.gl` all modules now p
+        - `react-map-gl` - is now written in typescript
+
+      - `deck.gl` 8.4
+        - 
+
+      - `luma.gl` 8.4
+        - TypeScript typings
+
+      - `math.gl` 3.4
+        - `@math.gl/geoid` - New module for working with Earth Gravity Models (micro-adjustments of ellipsoid)
+        - `@math.gl/polygon` - Improved support for binary polygons
+
+      <h2>vis.gl 8.3 Highlights</h2>
+
+      Release Date: ?
+      Updated: `deck.gl` 8.3, `luma.gl` 8.3 `loaders.gl` 2.3
+
+      <h2>vis.gl 8.2 Highlights</h2>
+
+      Release Date: ?
+      Updated: `deck.gl` 8.2, `luma.gl` 8.2 `loaders.gl` 2.2
+
     </div>
   );
 }
