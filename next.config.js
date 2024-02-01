44f7d01416e628969ccdb265ac935e14cbb6ad66
Ilya Boyandin <ilya@boyandin.me>
2024-02-01 23:33:57 +0100

    Static export (#82)
    * Static export
    
    * Trying to disable Netlify nextjs plugin
    
    * Added export build step
    
    * Added lighthouse plugin
    
    * Add NETLIFY_NEXT_PLUGIN_SKIP
    
    * Add NETLIFY_NEXT_PLUGIN_SKIP
    
    * Add NETLIFY_NEXT_PLUGIN_SKIP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    unoptimized: true
+  },
   webpack: configuration => {
     configuration.module.rules.push({
       test: /\.ya?ml$/,
