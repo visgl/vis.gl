9d5ba3e4d0ea6adf18f1273e1d6ecf0c6847ac55
Chris Gervang <chris@gervang.com>
2024-05-09 08:53:46 -0700

    Add probe.gl and deck.gl-community
    - fix mobile frameworks styles

--- next.config.js
+++ next.config.js
@@ -12,7 +12,13 @@ const nextConfig = {
     return configuration;
   },
   async redirects() {
-    return ['react-google-maps', 'react-map-gl', 'math.gl', 'deck.gl-community'].map(project => (
+    return [
+      'react-google-maps', 
+      'react-map-gl', 
+      'math.gl', 
+      'deck.gl-community', 
+      'probe.gl'
+    ].map(project => (
       {
         source: `/${project}`,
         destination: `https://visgl.github.io/${project}/`,
